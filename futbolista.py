99991883dc21a1dd50d77427eecf70c96cb2e7e0
Savarelav18 <111089079+Savarelav18@users.noreply.github.com>
2022-10-17 15:01:32 -0500

    Update futbolista.py

--- futbolista.py
+++ futbolista.py
@@ -12,7 +12,7 @@ class Futbolista(Persona,Deportista):
         self.listaFutbolistas.append(self)
     
     def __str__(self):
-        return f"Mi nombre es {Persona.getNombre()} soy profesional en el deporte {Deportista.getDeporte()} Tengo {Persona.getEdad()} años de edad y llevo {Deportista.getAñosPracticando()} años en el deporte"
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
     
     def getTarjetasRojas(self):
         return self._tarjetasRojas
